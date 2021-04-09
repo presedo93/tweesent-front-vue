5fa9770171c780d085876a1701f4ad384e9fdbbc
Cristian Pérez Romero <criiisperez@gmail.com>
2021-04-09 19:39:59 +0200

    💫 Añadido animaciones con gsap en el onmounted

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,14 +1,8 @@
 <template>
   <div>
-    <transition appear
-    @before-enter="beforeEnter"
-    :css="false">
-    <div>
-    <blockquote class="twitter-tweet" data-height="300px" data-conversation="none" data-cards="hidden" data-theme="dark">
-      <a :href="url"/>
-    </blockquote>
+    <div :id="identificador" class="card" :class="identificador">
+      <div class="card-body">{{ identificador }}{{ texto }}</div>
     </div>
-    </transition>
   </div>
 </template>
 
@@ -21,75 +15,80 @@ export interface TweetData {
   name: string;
   text: string;
   sentiment: string;
-  confidence: string
+  confidence: string;
 }
 
 export default defineComponent({
   props: {
     tweet: {
-      type : Object as PropType<TweetData>,
-      required : true
+      type: Object as PropType<TweetData>,
+      required: true,
     },
   },
 
   setup(props) {
-    const url = ref<string>("https://twitter.com/" + props.tweet.name + "/status/" + props.tweet.id);
+    const url = ref<string>(
+      "https://twitter.com/" + props.tweet.name + "/status/" + props.tweet.id
+    );
+    const texto = ref<string>(props.tweet.text);
+    const identificador = ref<string>(props.tweet.id);
+    const sentiment = ref<string>(props.tweet.sentiment);
+
+    function removeElement(elementToDelete: any) {
+      elementToDelete = document.getElementById(elementToDelete);
+      return function () {
+        debugger;
+        elementToDelete.parentElement.parentElement.remove();
+      };
+    }
+
+    function animationCard(el: any) {
+      let positionX = 0;
+      const positionY = 500;
+      if (sentiment.value == "positive") {
+        positionX = 500;
+      } else if (sentiment.value == "negative") {
+        positionX = -500;
+      } else {
+        positionX = 0;
+      }
+
+      const tl = gsap.timeline(); //create the timeline
+      tl.fromTo(
+        el,
+        { duration: 10, x: 0 },
+        {
+          duration: 5,
+          opacity: 1,
+          x: positionX,
+        }
+      )
+        .to(el, {
+          duration: 5,
+          opacity: 0,
+          y: positionY,
+        })
+        .call(removeElement(identificador.value));
+    }
 
     onMounted(() => {
-      const plugin = document.createElement("script");
-      plugin.setAttribute(
-        "src",
-        "https://platform.twitter.com/widgets.js",
-      );
-      plugin.async = true;
-      document.head.appendChild(plugin);
-    })
+      const element = document.getElementById(identificador.value);
+      animationCard(element);
+    });
 
-    function beforeEnter(el : any) {
-      const tl = gsap.timeline(); //create the timeline
-      tl.fromTo(el, { duration: 10, x:0 }, {
-        duration: 5,
-        opacity: 1,
-        x: 500,
-      }).to(el, {
-        duration: 5,
-        opacity: 0.2,
-        y: 500,
-      })
-    }
-
-    function enter(el : any, done: any) {
-      gsap.to(el, {
-        duration: 5,
-        scaleX: 1.5,
-        scaleY: 0.7,
-        opacity: 1,
-        x: 150,
-        ease: 'elastic.inOut(2.5, 1)',
-        onComplete: done
-      })
-    }
-
-    function leave(el : any, done: any) {
-      gsap.to(el, {
-        duration: 0.7,
-        scaleX: 1,
-        scaleY: 1,
-        x: 300,
-        ease: 'elastic.inOut(2.5, 1)'
-      })
-      gsap.to(el, {
-        duration: 0.2,
-        delay: 0.5,
-        opacity: 0,
-        onComplete: done
-      })
-    }
-
-    return { url, beforeEnter, enter, leave };
+    return {
+      url,
+      texto,
+      identificador,
+      animationCard,
+      removeElement,
+    };
   },
 });
 </script>
 
 <style lang="scss" scoped>
+.card-body {
+  color: black;
+}
 </style>
