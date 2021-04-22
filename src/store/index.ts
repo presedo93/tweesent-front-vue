692a6ab68e34dd2ef647d7d673d618d65486ce3e
Cristian Pérez Romero <criiisperez@gmail.com>
2021-04-22 16:40:39 +0200

    ✨Añadido store en contenedores para mostrar los tweets

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,20 +1,26 @@
 import { createStore } from "vuex";
+import { TweetData } from "@/components/Tweet.vue";
 
 export default createStore({
   state: {
     theme: "dark",
+    tweets: [] as TweetData[]
   },
   getters:{
     theme: state => state.theme,
+    tweets: state => state.tweets
   },
   mutations: {
     changeTheme(state){
-      debugger;
       if (state.theme === "dark") {
         state.theme = "light";
       } else {
         state.theme = "dark";
       }
+    },
+    addTweet(state, newTweets : TweetData){
+      const a = newTweets as TweetData;
+      state.tweets.push(a);
     }
   },
   actions: {},
